42d7f384634d74f1975b3aa93368628cf158d3ba
Tom Chambers <tom.chambers@gmail.com>
2014-09-02 20:58:43 +0100

    fixed typos

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,6 +48,78 @@ module.exports = function(grunt) {
 					'app/styles/main.css': 'app/styles/main.scss'
 				}
 			}
+		},
+		concat: {
+
+		},
+		'gh-pages': {
+			options: {
+				base: 'build'
+			},
+			src: ['**']
+		},
+		copy: {
+			build: {
+				cwd: 'app',
+				src: ['**'],
+				dest: 'build',
+				expand: true
+			}
+		},
+		autoprefixer: {
+			build: {
+				expand: true,
+				cwd: 'build',
+				src: ['**/*.css'],
+				dest: 'build'
+			}
+		},
+		useminPrepare: {
+			html: 'build/index.html',
+			options: {
+				dest: 'build'
+			}
+		},
+		cssmin: {
+			generated: {
+				files: {
+					'build/application.css': ['app/styles/*.css']
+				}
+			}
+		},
+		uglify: {
+			generated: {
+				options: {
+					mangle: false
+				},
+				files: {
+					'build/application.js': ['app/scripts/*.js']
+				}
+			}
+		},
+		usemin: {
+			html: 'build/index.html'
+		},
+		imagemin: {
+			build: {
+				files: [{
+					expand: true,
+					cwd: 'app/images',
+					src: '{,*/}*.{png,jpg,jpeg,gif}',
+					dest: 'build/images'
+				}]
+			}
+		},
+		clean: {
+			build: {
+				src: ['build/']
+			},
+			stylesheets: {
+				src: ['build/styles/*.css', '!build/styles/application.css']
+			},
+			scripts: {
+				src: ['build/scripts/*.js', '!build/scripts/application.js']
+			}
 		}
 	});
 
@@ -55,10 +127,21 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-gh-pages');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-usemin');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 
 	grunt.event.on('watch', function(action,filepath,target) {
 		grunt.log.writeln(target + ': '+filepath+' has '+action);
 	});
 
-	grunt.registerTask('serve', ['watch','connect'])
+	grunt.registerTask('serve', ['watch','connect']);
+	grunt.registerTask('build', ['clean:build','sass','copy','autoprefixer','useminPrepare','concat','cssmin','uglify','usemin','imagemin','clean:stylesheets','clean:scripts','gh-pages']);
 }
